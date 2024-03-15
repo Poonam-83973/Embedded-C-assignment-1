65a160c4aaafbe61591d7392a2dbd869113df6b0
poonamkokitkar <poonamkokitkar19@gmail.com>
2024-03-15 14:33:06 +0530

    Q5 & Q11

--- 11.c
+++ 11.c
@@ -3,10 +3,15 @@
 	unsigned, by printing appropriate values from standard headers.
 	Do not use spaces to align table columns.
 	(note: the size and range changes from compiler to compiler)
+
+	Q12. Extend above program (11) to print ranges for float, double, and long double. Use <float.h>.
+	“float” numbers can be printed using %f, %g, %e, %E.
 */
 
 #include<stdio.h>
 #include<limits.h>
+#include<float.h>
+
 int main()
 {
 	printf("------------------------------------------------------\n");
@@ -20,6 +25,9 @@ int main()
 	printf("unsigned int       %d           %%u         0 to %u  \n",sizeof(unsigned int), UINT_MAX);
 	printf("long int           %d           %%ld       %ld to %ld \n",sizeof(long int),LONG_MIN, LONG_MAX);
 	printf("unsigned long int  %d           %%lu        0 to %lu \n",sizeof(unsigned long int), ULONG_MAX);
+	printf("float              %d           %%f        %f to %f \n",sizeof(float), FLT_MIN, FLT_MAX);
+	printf("double             %d           %%lf        %lf to %lf \n",sizeof(double), DBL_MIN, DBL_MAX);
+	printf("long double        %d           %%Lf       %Lf to %Lf \n",sizeof(long double), LDBL_MIN, LDBL_MAX);
 	return 0; 
 }
 
